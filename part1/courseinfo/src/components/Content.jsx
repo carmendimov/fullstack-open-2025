682e245f53c204de59e3c2907f6346339661d83c
Carmen Dimov <e12216470@student.tuwien.ac.at>
2025-12-19 06:00:23 +0100

    part1: add courseinfo

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,17 @@
+const Content = () => {
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  )
+}
+
+export default Content
