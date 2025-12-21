5662828d3148a1f54e290d48afb038a38b016a94
Carmen Dimov <e12216470@student.tuwien.ac.at>
2025-12-21 02:59:46 +0100

    part2: add part2a

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,37 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return <b>total of {total} exercises</b>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
