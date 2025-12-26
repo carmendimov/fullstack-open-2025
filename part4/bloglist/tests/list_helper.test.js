c8aa6d3e57a3747ef1c8eaad1223081818170ff0
Carmen Dimov <e12216470@student.tuwien.ac.at>
2025-12-26 16:33:21 +0100

    part4a: step 3

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -64,3 +64,58 @@ describe('total likes', () => {
     assert.strictEqual(result, 10)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ]
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Title 2',
+      author: 'Author 2',
+      url: 'Url 2',
+      likes: 8,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d1710',
+      title: 'Title 3',
+      author: 'Author 3',
+      url: 'Url 3',
+      likes: 3,
+      __v: 0,
+    },
+  ]
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, null)
+  })
+
+  test('when list has only one blog, returns that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[0])
+  })
+
+  test('of a bigger list returns the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+    assert.deepStrictEqual(result, listWithMultipleBlogs[1])
+  })
+})
