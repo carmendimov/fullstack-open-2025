0faa3df5dfa884737630782d4cacecfa78c49735
Carmen Dimov <e12216470@student.tuwien.ac.at>
2025-12-27 18:23:52 +0100

    part4b: add step 2 and 3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -24,6 +24,41 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('unique identifier property of blog is named id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach((blog) => {
+    assert(blog.id !== undefined)
+  })
+})
+
+test('a blog post can be added', async () => {
+  const newBlog = {
+    title: 'The Rust Programming Language',
+    author: 'Steve Klabnik',
+    url: 'https://doc.rust-lang.org/book/',
+    likes: 42,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtTEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtTEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsAtTEnd.map((b) => b.title)
+  console.log(titles)
+
+  assert(titles.includes(newBlog.title))
+
+  const saved = blogsAtTEnd.find((b) => b.title === newBlog.title)
+  assert.strictEqual(saved.author, newBlog.author)
+  assert.strictEqual(saved.url, newBlog.url)
+  assert.strictEqual(saved.likes, newBlog.likes)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
--- part4/bloglist/tests/blog_test_helper.js
+++ part4/bloglist/tests/blog_test_helper.js
@@ -1,20 +1,26 @@
 const Blog = require('../models/blog')
 
 const initialBlogs = [
-	{
+  {
     title: 'Java ist auch eine Insel',
     author: 'Christian Ullenboom',
     url: 'https://openbook.rheinwerk-verlag.de/javainsel/',
-		likes: 7,
-	},
-	{
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-		likes: 5,
-	},
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+  },
 ]
 
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map((blog) => blog.toJSON())
+}
+
 module.exports = {
-	initialBlogs,
+  initialBlogs,
+  blogsInDb,
 }
