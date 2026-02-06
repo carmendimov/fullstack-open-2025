045c6e943f4f193702f89c5db24dc3673c24ec05
Carmen Dimov <e12216470@student.tuwien.ac.at>
2026-02-06 16:25:00 +0100

    part7: add 7.7

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -11,14 +11,36 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) {
+      setCountry(null)
+      return
+    }
+
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then((response) => {
+        const data = response.data
+        const countryData = {
+          name: data.name.common,
+          capital: data.capital[0],
+          population: data.population,
+          flag: data.flags.png,
+        }
+
+        setCountry({ found: true, data: countryData })
+      })
+      .catch(() => {
+        setCountry({ found: false })
+      })
+  }, [name])
 
   return country
 }
@@ -29,19 +51,19 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
   )
 }
